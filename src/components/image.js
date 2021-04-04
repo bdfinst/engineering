700d7879c04d3ee9c439c3409c8d700040b8f2e0
Bryan Finster <bryan.finster@gmail.com>
2021-04-04 17:02:48 -0500

    Linting

--- src/components/image.js
+++ src/components/image.js
@@ -5,16 +5,19 @@ import React from 'react'
 
 const Image = () => (
   <StaticQuery
-    query={graphql`{
-  placeholderImage: file(relativePath: {eq: "gatsby-icon.png"}) {
-    childImageSharp {
-      gatsbyImageData(width: 300, layout: CONSTRAINED)
-    }
-  }
-}
-`}
+    query={graphql`
+      {
+        placeholderImage: file(relativePath: { eq: "gatsby-icon.png" }) {
+          childImageSharp {
+            gatsbyImageData(width: 300, layout: CONSTRAINED)
+          }
+        }
+      }
+    `}
     render={data => (
-      <GatsbyImage fluid={data.placeholderImage.childImageSharp.gatsbyImageData} />
+      <GatsbyImage
+        fluid={data.placeholderImage.childImageSharp.gatsbyImageData}
+      />
     )}
   />
 )
--- src/components/layout.js
+++ src/components/layout.js
@@ -12,41 +12,39 @@ import { StaticQuery, graphql } from 'gatsby'
 import Header from './header'
 import './layout.css'
 
-const Layout = ({ children }) => {
-  return (
-    <StaticQuery
-      query={graphql`
-        query SiteTitleQuery {
-          site {
-            siteMetadata {
-              title
-            }
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
           }
         }
-      `}
-      render={data => (
-        <div style={{ minHeight: '100vh', backgroundColor: '#FFF' }}>
-          <Header siteTitle={data.site.siteMetadata.title} />
-          <div
-            style={{
-              margin: `0 auto`,
-              maxWidth: 960,
-              padding: `0px 1.0875rem 1.45rem`,
-              paddingTop: 100,
-            }}
-          >
-            <main>{children}</main>
-            <footer style={{ paddingTop: 10 }}>
-              © {new Date().getFullYear()}, Built with
-              {` `}
-              <a href="https://www.gatsbyjs.org">Gatsby</a>
-            </footer>
-          </div>
+      }
+    `}
+    render={data => (
+      <div style={{ minHeight: '100vh', backgroundColor: '#FFF' }}>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 100,
+          }}
+        >
+          <main>{children}</main>
+          <footer style={{ paddingTop: 10 }}>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
         </div>
-      )}
-    />
-  )
-}
+      </div>
+    )}
+  />
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
