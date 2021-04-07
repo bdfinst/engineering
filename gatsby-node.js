b7e6c9aa7a981e47d63b9f6e758351f50fb6f78b
Bryan Finster <bryan.finster@leapfrog.ai>
2021-04-07 14:07:53 -0500

    Removing cruftx

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,47 +14,6 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   }
 }
 
-// const createTagPages = (createPage, posts) => {
-//   const allTagsIndexTemplate = path.resolve('src/templates/allTagsIndex.js')
-//   const singleTagIndexTemplate = path.resolve('src/templates/singleTagIndex.js')
-
-//   const postsByTag = {}
-
-//   posts.forEach(({ node }) => {
-//     if (node.frontmatter.tags) {
-//       node.frontmatter.tags.forEach(tag => {
-//         if (!postsByTag[tag]) {
-//           postsByTag[tag] = []
-//         }
-//         postsByTag[tag].push({ node })
-//       })
-//     }
-//   })
-
-//   const tags = Object.keys(postsByTag)
-
-//   createPage({
-//     path: '/tags',
-//     component: allTagsIndexTemplate,
-//     context: {
-//       tags: tags.sort(),
-//     },
-//   })
-
-//   tags.forEach(tagName => {
-//     const _posts = postsByTag[tagName]
-
-//     createPage({
-//       path: `/tags/${tagName}`,
-//       component: singleTagIndexTemplate,
-//       context: {
-//         posts: _posts,
-//         tagName,
-//       },
-//     })
-//   })
-// }
-
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   return new Promise(resolve => {
@@ -81,15 +40,11 @@ exports.createPages = ({ graphql, actions }) => {
     `).then(result => {
       const posts = result.data.allMarkdownRemark.edges
 
-      // createTagPages(createPage, posts)
-
       posts.forEach(({ node }) => {
         if (!node.frontmatter.title) {
           throw new Error(`No title for ${node.fields.slug}`)
         }
-        if (!node.frontmatter.tags || !node.frontmatter.tags.length) {
-          throw new Error(`No title for ${node.fields.slug}`)
-        }
+
         createPage({
           path: node.fields.slug,
           component: path.resolve(`./src/templates/playbook.js`),
